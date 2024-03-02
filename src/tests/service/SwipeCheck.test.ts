379d38113eb9dc19ac5aadae71a3e6e1dad1e021
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-03-02 21:17:53 +0900

    スワイプ、マウス操作に関してテストまで完了

--- src/tests/service/SwipeCheck.test.ts
+++ src/tests/service/SwipeCheck.test.ts
@@ -2,39 +2,63 @@ import swipeCheck from "../../service/SwipeCheck.ts";
 import React from "react";
 
 describe('SwipeCheck.tsのテスト', () => {
-    test('setStartYにeventを受け取ると、メンバー変数のstartYが置き換わる', () => {
-        const event =
-            {
-                targetTouches:
-                    [{
-                        clientX: 100,
-                        clientY: 200
-                    }]
-            } as unknown as React.TouchEvent<HTMLDivElement>
-        swipeCheck.setStartYByTouch(event)
+    describe('スワイプ操作に関して', () => {
+        test('setStartYByTouchにeventを受け取ると、メンバー変数のstartYが置き換わる', () => {
+            const event =
+                {
+                    targetTouches:
+                        [{
+                            clientX: 100,
+                            clientY: 200
+                        }]
+                } as unknown as React.TouchEvent<HTMLDivElement>
+            swipeCheck.setStartYByTouch(event)
 
-        expect(swipeCheck.startY).toBe(200)
-    })
-    test('setEndYにeventを受け取ると、メンバー変数のendYが置き換わる', () => {
-        const event =
-            {
-                targetTouches:
-                    [{
-                        clientX: 100,
-                        clientY: 200
-                    }]
-            } as unknown as React.TouchEvent<HTMLDivElement>
-        swipeCheck.setEndYByTouch(event)
+            expect(swipeCheck.startY).toBe(200)
+        })
+        test('setEndYByTouchにeventを受け取ると、メンバー変数のendYが置き換わる', () => {
+            const event =
+                {
+                    targetTouches:
+                        [{
+                            clientX: 100,
+                            clientY: 200
+                        }]
+                } as unknown as React.TouchEvent<HTMLDivElement>
+            swipeCheck.setEndYByTouch(event)
 
-        expect(swipeCheck.endY).toBe(200)
+            expect(swipeCheck.endY).toBe(200)
+        })
     })
-    test('swipeCheckメソッドを呼び出した際に、s；endYがstartYより大きかった場合だけtrueを返す',()=>{
+    describe('マウス操作に関して', () => {
+        test('setStartYByMouseにeventを受け取ると、メンバー変数のstartYが置き換わる', () => {
+            const event =
+                {
+                            clientX: 100,
+                            clientY: 200
+                } as unknown as React.MouseEvent<HTMLDivElement>
+            swipeCheck.setStartYByMouse(event)
+
+            expect(swipeCheck.startY).toBe(200)
+        })
+        test('setEndYByMouseにeventを受け取ると、メンバー変数のendYが置き換わる', () => {
+            const event =
+                {
+                            clientX: 100,
+                            clientY: 200
+                } as unknown as React.MouseEvent<HTMLDivElement>
+            swipeCheck.setEndYByMouse(event)
+
+            expect(swipeCheck.endY).toBe(200)
+        })
+    })
+    test('swipeCheckメソッドを呼び出した際に、s；endYがstartYより大きかった場合だけtrueを返す', () => {
         swipeCheck.startY = 0
-        swipeCheck.endY =10
+        swipeCheck.endY = 10
 
         expect(swipeCheck.swipeCheck()).toBe(true)
 
-        swipeCheck.startY  =100
+        swipeCheck.startY = 100
 
         expect(swipeCheck.swipeCheck()).toBe(false)
     })
